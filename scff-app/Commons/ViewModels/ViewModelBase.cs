b3f8514849576c93501adf2a72bb841179152df8
progremaster <djyayutto@gmail.com>
2012-05-13 07:01:34 +0900

    #6 レイアウトウィンドウの実装 マージミスを修正

---
+++ scff-app/Commons/ViewModels/ViewModelBase.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.Contracts;
+
+namespace ScffApp.Commons.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged メンバー
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            Contract.Requires(GetType().GetProperty(propertyName) != null);
+
+            if (PropertyChanged == null)
+            {
+                return;
+            }
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

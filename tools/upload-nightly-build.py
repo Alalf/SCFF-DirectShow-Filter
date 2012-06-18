68fb09db922285249d1c6671e92b8cac6a5bcf03
Alalf <alalf.iQLc@gmail.com>
2012-06-19 06:31:46 +0900

    タイムスタンプをつけるように更新。

--- tools/upload-nightly-build.py
+++ tools/upload-nightly-build.py
@@ -1,11 +1,15 @@
 ﻿import shutil
+import datetime
 
 # 定数
 k32bitDir = 'SCFF-x86'
 k64bitDir = 'SCFF-amd64'
 
+kToday = datetime.datetime.today()
+kTimestamp = kToday.strftime("%Y%m%d-%H%M%S")
+
 # パッケージ作成
-shutil.make_archive('..\\package\\' + k32bitDir, 'zip', '..\\package\\' + k32bitDir)
-shutil.make_archive('..\\package\\' + k64bitDir, 'zip', '..\\package\\' + k64bitDir)
+shutil.make_archive('..\\package\\' + k32bitDir + "-" + kTimestamp, 'zip', '..\\package\\' + k32bitDir)
+shutil.make_archive('..\\package\\' + k64bitDir + "-" + kTimestamp, 'zip', '..\\package\\' + k64bitDir)
 
 # GitHubにアクセス

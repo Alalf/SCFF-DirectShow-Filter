3bb1c7e9008f770d7fc1a11d365d3a6ceeda1e97
Alalf <alalf.iQLc@gmail.com>
2012-06-19 06:08:52 +0900

    githubアクセスをpythonライブラリではなくcurlに任せる方向で模索中。

---
+++ tools/upload-nightly-build.py
@@ -0,0 +1,11 @@
+﻿import shutil
+
+# 定数
+k32bitDir = 'SCFF-x86'
+k64bitDir = 'SCFF-amd64'
+
+# パッケージ作成
+shutil.make_archive('..\\package\\' + k32bitDir, 'zip', '..\\package\\' + k32bitDir)
+shutil.make_archive('..\\package\\' + k64bitDir, 'zip', '..\\package\\' + k64bitDir)
+
+# GitHubにアクセス

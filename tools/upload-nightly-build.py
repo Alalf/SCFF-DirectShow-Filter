12a622ada53cafac9950f60fa4743318605763df
Alalf <alalf.iQLc@gmail.com>
2012-06-19 22:37:18 +0900

    tools/upload-nightly-build.pyを作りかけ。AmazonS3へのアップロードがうまくいかない。要調査。

--- tools/upload-nightly-build.py
+++ tools/upload-nightly-build.py
@@ -1,38 +1,82 @@
-﻿import shutil
+﻿#----------------------------------------------------------------------
+# アーカイブ作成
+#----------------------------------------------------------------------
+import shutil
 import datetime
+import os.path
 
 # 定数
-k32bitDir = 'SCFF-x86'
-k64bitDir = 'SCFF-amd64'
+k32bitDir = 'SCFF-DirectShow-Filter-x86'
+k64bitDir = 'SCFF-DirectShow-Filter-amd64'
 
 kToday = datetime.datetime.today()
 kTimestamp = kToday.strftime("%Y%m%d-%H%M%S")
 
 # パッケージ作成
-shutil.make_archive('..\\package\\' + k32bitDir + "-" + kTimestamp, 'zip', '..\\package\\' + k32bitDir)
-shutil.make_archive('..\\package\\' + k64bitDir + "-" + kTimestamp, 'zip', '..\\package\\' + k64bitDir)
+path_32bit = shutil.make_archive('..\\package\\' + k32bitDir + "-" + kTimestamp, 'zip', '..\\package\\' + k32bitDir)
+path_64bit = shutil.make_archive('..\\package\\' + k64bitDir + "-" + kTimestamp, 'zip', '..\\package\\' + k64bitDir)
 
-# GitHubにアクセス
-#
-# GitHub API v3勉強の記録
-# ---------------------------------------------------------------------
-# 1. パスワードを毎回打ち込むのがめんどくさければOAuth2 Tokenを作る
-#   curl -u "Alalf" -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d "{\"scopes\":[\"SCFF-DirectShow-Filter\"],\"note\":\"upload-nightly-build.py\"}" https://api.github.com/authorizations
-# 返ってきたメッセージのTokenを使えば以降URLにパスワード入力なしでアクセス可能:
-#   https://api.github.com?access_token=TOKEN
-# 言うまでも無いが、このTokenはパスワードと同じぐらい注意して取り扱うこと。
-# どうみてもアクセス制限が全く無い！なおTokenを忘れたときは:
-#   curl -u "Alalf" -i https://api.github.com/authorizations
-#
-# 2. Downloadsのリストを取ってきてみる
-#   curl -u "Alalf" https://api.github.com/repos/Alalf/SCFF-DirectShow-Filter/downloads
-# のちのち、必要の無くなったビルドを消していく予定
-#
-# 3. まずはリソースを作成する
-#   curl -u "Alalf" -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d "{\"name\":\"test.zip\",\"size\":118}" https://api.github.com/repos/Alalf/SCFF-DirectShow-Filter/downloads
-# このレスポンスの内容から、アップロードに必要な情報を抜き出す
-#
-# 4. 内容をアップロードする
-#   curl -u "Alalf" -i -H "Accept: application/json" -H "Content-Type: application/json" -F "key=downloads/Alalf/SCFF-DirectShow-Filter/test.zip" -F "acl=public-read" -F "success_action_status=201" -F "Filename=test.zip" -F "AWSAccessKeyId=XXX" -F "Policy=XXX" -F "Signature=XXX" -F "Content-Type=application/zip" -F "file=@D:\\Private\\Desktop\\test.zip" https://github.s3.amazonaws.com/
-# こんな感じかな？まだ動かないので要調査
-# それにしてもめんどくさいなー
+# ファイル名とサイズを取得
+filename_32bit = os.path.split(path_32bit)[1]
+filesize_32bit = os.path.getsize(path_32bit)
+filename_64bit = os.path.split(path_64bit)[1]
+filesize_64bit = os.path.getsize(path_64bit)
+
+print filename_32bit
+print filesize_32bit
+print filename_64bit
+print filesize_64bit
+
+#----------------------------------------------------------------------
+# GitHubにアップロード
+#----------------------------------------------------------------------
+
+import requests
+import json
+import time
+import calendar
+
+# 定数
+
+kUserName = 'Alalf'
+kPassword = raw_input('GitHub Password: ')
+kAuth = (kUserName, kPassword)
+
+kDownloads = 'https://api.github.com/repos/Alalf/SCFF-DirectShow-Filter/downloads'
+
+# ダウンロードリスト取得
+#response = requests.get(kDownloads, auth=kAuth)
+#if response.ok:
+#	data = response.json
+#	print json.dumps(data, indent=2)
+#	# あとはもうdataから直接値を取り出せる
+#	# data[0]["url"] etc.
+
+# 参考:
+# http://docs.python-requests.org/en/latest/user/quickstart/#more-complicated-post-requests
+
+# アップロード準備
+headers = {'content-type':'application/json', 'accept':'application/json'}
+payload = {'name':filename_32bit, 'size':filesize_32bit}
+response = requests.post(kDownloads, auth=kAuth, headers=headers, data=json.dumps(payload))
+if not response.ok:
+	print "Create download error!"
+	quit()
+data = response.json
+print json.dumps(data, indent=2)
+
+# Expiresを計算
+expires = calendar.timegm(time.strptime(data['expirationdate'],'%Y-%m-%dT%H:%M:%S.000Z'))
+
+# 実際にファイルをアップロード
+payload = [('key',data['path']), ('acl',data['acl']), ('success_action_status',201), ('Filename',data['name']), ('AWSAccessKeyId',data['accesskeyid']), ('Policy',data['policy']), ('Signature',data['signature']), ('Expires',expires), ('Content-Type',data['mime_type']), ('file',open(path_32bit, 'rb'))]
+#print payload
+#files = {'file': (filename_32bit, open(path_32bit, 'rb'))}
+response = requests.post(data['s3_url'], data=payload)
+print response.content
+
+if not response.ok:
+	print "Create download error!"
+	quit()
+
+print 'ALL OK'

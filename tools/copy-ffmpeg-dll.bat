ff71800821f36216ab07743aea910e8e8b3f0450
Alalf <alalf.iQLc@gmail.com>
2012-07-02 04:38:06 +0900

    SCFFではasyncオプションが入らない気がするので消去

--- tools/copy-ffmpeg-dll.bat
+++ tools/copy-ffmpeg-dll.bat
@@ -1,26 +1,26 @@
-@echo off
-set ROOT_DIR=%~dp0..\
-pushd "%ROOT_DIR%"
+@ECHO OFF
+SET ROOT_DIR=%~dp0..\
+PUSHD "%ROOT_DIR%"
 
-mkdir "dist\Debug-amd64\"
-mkdir "dist\Release-amd64\"
-mkdir "dist\Debug-x86\"
-mkdir "dist\Release-x86\"
+MKDIR "dist\Debug-amd64\"
+MKDIR "dist\Release-amd64\"
+MKDIR "dist\Debug-x86\"
+MKDIR "dist\Release-x86\"
 
-copy /y "ext\ffmpeg\amd64\bin\avcodec*.dll" "dist\Debug-amd64\"
-copy /y "ext\ffmpeg\amd64\bin\avutil*.dll" "dist\Debug-amd64\"
-copy /y "ext\ffmpeg\amd64\bin\swscale*.dll" "dist\Debug-amd64\"
+COPY /Y "ext\ffmpeg\amd64\bin\avcodec*.dll" "dist\Debug-amd64\"
+COPY /Y "ext\ffmpeg\amd64\bin\avutil*.dll" "dist\Debug-amd64\"
+COPY /Y "ext\ffmpeg\amd64\bin\swscale*.dll" "dist\Debug-amd64\"
 
-copy /y "ext\ffmpeg\amd64\bin\avcodec*.dll" "dist\Release-amd64\"
-copy /y "ext\ffmpeg\amd64\bin\avutil*.dll" "dist\Release-amd64\"
-copy /y "ext\ffmpeg\amd64\bin\swscale*.dll" "dist\Release-amd64\"
+COPY /Y "ext\ffmpeg\amd64\bin\avcodec*.dll" "dist\Release-amd64\"
+COPY /Y "ext\ffmpeg\amd64\bin\avutil*.dll" "dist\Release-amd64\"
+COPY /Y "ext\ffmpeg\amd64\bin\swscale*.dll" "dist\Release-amd64\"
 
-copy /y "ext\ffmpeg\x86\bin\avcodec*.dll" "dist\Debug-x86\"
-copy /y "ext\ffmpeg\x86\bin\avutil*.dll" "dist\Debug-x86\"
-copy /y "ext\ffmpeg\x86\bin\swscale*.dll" "dist\Debug-x86\"
+COPY /Y "ext\ffmpeg\x86\bin\avcodec*.dll" "dist\Debug-x86\"
+COPY /Y "ext\ffmpeg\x86\bin\avutil*.dll" "dist\Debug-x86\"
+COPY /Y "ext\ffmpeg\x86\bin\swscale*.dll" "dist\Debug-x86\"
 
-copy /y "ext\ffmpeg\x86\bin\avcodec*.dll" "dist\Release-x86\"
-copy /y "ext\ffmpeg\x86\bin\avutil*.dll" "dist\Release-x86\"
-copy /y "ext\ffmpeg\x86\bin\swscale*.dll" "dist\Release-x86\"
+COPY /Y "ext\ffmpeg\x86\bin\avcodec*.dll" "dist\Release-x86\"
+COPY /Y "ext\ffmpeg\x86\bin\avutil*.dll" "dist\Release-x86\"
+COPY /Y "ext\ffmpeg\x86\bin\swscale*.dll" "dist\Release-x86\"
 
-popd
+POPD

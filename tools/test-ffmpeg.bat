5740cdb35f0f43620b052f2bb6b6a85412ef89e4
Alalf <alalf.iQLc@gmail.com>
2012-07-28 19:16:30 +0900

    asyncのかわりにvf_fpsを導入

--- tools/test-ffmpeg.bat
+++ tools/test-ffmpeg.bat
@@ -10,7 +10,7 @@ SET FFMPEG_EXE=ext\ffmpeg\amd64\bin\ffmpeg.exe
 
 MKDIR "%OUTPUT_DIR%"
 DEL "%OUTPUT%"
-"%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -f dshow -s 640x360 -i video="%VIDEO%":audio="%AUDIO%" -r 30 -s 640x360 -vcodec libx264 -preset medium -x264opts b-adapt=2:direct=auto:keyint=300:me=umh:rc-lookahead=50:ref=6:subme=5 -maxrate 1200k -bufsize 2400k -crf 23 -qmin 10 -qmax 51 -acodec libvo_aacenc -ar 44100 -ab 96k -async 100 -ac 2 -threads 4 -f flv "%OUTPUT%"
+"%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -f dshow -s 640x360 -i video="%VIDEO%":audio="%AUDIO%" -r 30 -s 640x360 -vcodec libx264 -preset medium -x264opts b-adapt=2:direct=auto:keyint=300:me=umh:rc-lookahead=50:ref=6:subme=5 -maxrate 1200k -bufsize 2400k -crf 23 -qmin 10 -qmax 51 -vf fps="fps=30" -acodec libvo_aacenc -ar 44100 -ab 96k -ac 2 -threads 4 -f flv "%OUTPUT%"
 "%OUTPUT%"
 
 POPD

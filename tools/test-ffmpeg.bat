91b39198b1e92ff6035bed3c19d7fcc882b13cf8
Alalf <alalf.iQLc@gmail.com>
2012-07-28 20:22:22 +0900

    ffmpegの設定をcavelisさんにおしえてもらった設定からアレンジ

--- tools/test-ffmpeg.bat
+++ tools/test-ffmpeg.bat
@@ -10,7 +10,9 @@ SET FFMPEG_EXE=ext\ffmpeg\amd64\bin\ffmpeg.exe
 
 MKDIR "%OUTPUT_DIR%"
 DEL "%OUTPUT%"
-"%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -f dshow -s 640x360 -i video="%VIDEO%":audio="%AUDIO%" -r 30 -s 640x360 -vcodec libx264 -preset medium -x264opts b-adapt=2:direct=auto:keyint=300:me=umh:rc-lookahead=50:ref=6:subme=5 -maxrate 1200k -bufsize 2400k -crf 23 -qmin 10 -qmax 51 -vf fps="fps=30" -acodec libvo_aacenc -ar 44100 -ab 96k -ac 2 -threads 4 -f flv "%OUTPUT%"
+"%FFMPEG_EXE%" -rtbufsize 10MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -threads 4 -vf fps="fps=30" -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 1200k -bufsize 2400k -crf 23 -qmin 10 -qmax 51 -vcodec libx264 -preset medium -x264opts b-adapt=2:direct=auto:keyint=300:me=umh:rc-lookahead=50:ref=6:subme=5  -async 100 -acodec libvo_aacenc -ar 48000 -ab 96k -ac 2 -vol 256 -f flv "%OUTPUT%"
 "%OUTPUT%"
+
+REM "%FFMPEG_EXE%" -rtbufsize 10MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -threads 4 -vf fps="fps=30" -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 850k -bufsize 850k -crf 30 -vcodec libx264 -preset slow -profile:v main -async 100 -acodec libvo_aacenc -ar 48000 -ab 96k -ac 2 -vol 256 -f flv "%OUTPUT%"
 
 POPD

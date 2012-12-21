af18b7690798511d44b72e1583a9f7fbd5e5b096
Alalf <alalf.iQLc@gmail.com>
2012-12-22 04:45:29 +0900

    冷静に考えるとEnumはいらなかったので消去

--- SCFF.Common/OptionsINIFile.cs
+++ SCFF.Common/OptionsINIFile.cs
@@ -31,71 +31,6 @@ public static class OptionsINIFile {
   private const string OptionsFilePath = "SCFF.Common.Options.ini";
   private const string OptionsHeader = "; SCFF-DirectShow-Filter Options Ver.0.1.7";
 
-  /// ファイル入出力に用いるキー
-  private enum Key {
-    // Recent Profiles
-    RecentProfile1,
-    RecentProfile2,
-    RecentProfile3,
-    RecentProfile4,
-    RecentProfile5,
-
-    // FFmpeg Options
-    FFmpegPath,
-    FFmpegArguments,
-
-    // MainWindow
-    TmpMainWindowLeft,
-    TmpMainWindowTop,
-    TmpMainWindowWidth,
-    TmpMainWindowHeight,
-    TmpMainWindowState,
-
-    // MainWindow Expanders
-    TmpAreaIsExpanded,
-    TmpOptionsIsExpanded,
-    TmpResizeMethodIsExpanded,
-    TmpLayoutIsExpanded,
-
-    // SCFF Options
-    AutoApply,
-    LayoutPreview,
-    LayoutBorder,
-    LayoutSnap,
-
-    // SCFF Menu Options
-    TmpCompactView,
-    ForceAeroOn,
-    TmpRestoreLastProfile
-  }
-
-  /// Key(Enum)->String変換用の辞書。リフレクション利用回避のためわざわざ作成した。
-  private static readonly Dictionary<Key, string> keyToLabel = new Dictionary<Key, string>() {
-    {Key.RecentProfile1, "RecentProfile1"},
-    {Key.RecentProfile2, "RecentProfile2"},
-    {Key.RecentProfile3, "RecentProfile3"},
-    {Key.RecentProfile4, "RecentProfile4"},
-    {Key.RecentProfile5, "RecentProfile5"},
-    {Key.FFmpegPath, "FFmpegPath"},
-    {Key.FFmpegArguments, "FFmpegArguments"},
-    {Key.TmpMainWindowLeft, "TmpMainWindowLeft"},
-    {Key.TmpMainWindowTop, "TmpMainWindowTop"},
-    {Key.TmpMainWindowWidth, "TmpMainWindowWidth"},
-    {Key.TmpMainWindowHeight, "TmpMainWindowHeight"},
-    {Key.TmpMainWindowState, "TmpMainWindowState"},
-    {Key.TmpAreaIsExpanded, "TmpAreaIsExpanded"},
-    {Key.TmpOptionsIsExpanded, "TmpOptionsIsExpanded"},
-    {Key.TmpResizeMethodIsExpanded, "TmpResizeMethodIsExpanded"},
-    {Key.TmpLayoutIsExpanded, "TmpLayoutIsExpanded"},
-    {Key.AutoApply, "AutoApply"},
-    {Key.LayoutPreview, "LayoutPreview"},
-    {Key.LayoutBorder, "LayoutBorder"},
-    {Key.LayoutSnap, "LayoutSnap"},
-    {Key.TmpCompactView, "TmpCompactView"},
-    {Key.ForceAeroOn, "ForceAeroOn"},
-    {Key.TmpRestoreLastProfile, "TmpRestoreLastProfile"},
-  };
-
   //===================================================================
   // internalメソッド
   //===================================================================
@@ -105,29 +40,28 @@ public static class OptionsINIFile {
     using (var writer = new StreamWriter(OptionsFilePath)) {
       try {
         writer.WriteLine(OptionsHeader);
-        writer.WriteLine(keyToLabel[Key.RecentProfile1] + "=" + options.GetRecentProfile(0));
-        writer.WriteLine(keyToLabel[Key.RecentProfile2] + "=" + options.GetRecentProfile(1));
-        writer.WriteLine(keyToLabel[Key.RecentProfile3] + "=" + options.GetRecentProfile(2));
-        writer.WriteLine(keyToLabel[Key.RecentProfile4] + "=" + options.GetRecentProfile(3));
-        writer.WriteLine(keyToLabel[Key.RecentProfile5] + "=" + options.GetRecentProfile(4));
-        writer.WriteLine(keyToLabel[Key.FFmpegPath] + "=" + options.FFmpegPath);
-        writer.WriteLine(keyToLabel[Key.FFmpegArguments] + "=" + options.FFmpegArguments);
-        writer.WriteLine(keyToLabel[Key.TmpMainWindowLeft] + "=" + options.TmpMainWindowLeft);
-        writer.WriteLine(keyToLabel[Key.TmpMainWindowTop] + "=" + options.TmpMainWindowTop);
-        writer.WriteLine(keyToLabel[Key.TmpMainWindowWidth] + "=" + options.TmpMainWindowWidth);
-        writer.WriteLine(keyToLabel[Key.TmpMainWindowHeight] + "=" + options.TmpMainWindowHeight);
-        writer.WriteLine(keyToLabel[Key.TmpMainWindowState] + "=" + (int)options.TmpMainWindowState);
-        writer.WriteLine(keyToLabel[Key.TmpAreaIsExpanded] + "=" + options.TmpAreaIsExpanded);
-        writer.WriteLine(keyToLabel[Key.TmpOptionsIsExpanded] + "=" + options.TmpOptionsIsExpanded);
-        writer.WriteLine(keyToLabel[Key.TmpResizeMethodIsExpanded] + "=" + options.TmpResizeMethodIsExpanded);
-        writer.WriteLine(keyToLabel[Key.TmpLayoutIsExpanded] + "=" + options.TmpLayoutIsExpanded);
-        writer.WriteLine(keyToLabel[Key.AutoApply] + "=" + options.AutoApply);
-        writer.WriteLine(keyToLabel[Key.LayoutPreview] + "=" + options.LayoutPreview);
-        writer.WriteLine(keyToLabel[Key.LayoutBorder] + "=" + options.LayoutBorder);
-        writer.WriteLine(keyToLabel[Key.LayoutSnap] + "=" + options.LayoutSnap);
-        writer.WriteLine(keyToLabel[Key.TmpCompactView] + "=" + options.TmpCompactView);
-        writer.WriteLine(keyToLabel[Key.ForceAeroOn] + "=" + options.ForceAeroOn);
-        writer.WriteLine(keyToLabel[Key.TmpRestoreLastProfile] + "=" + options.TmpRestoreLastProfile);
+        string prefix = "RecentProfile";
+        for (int i = 0; i < 5; ++i) {
+          writer.WriteLine(prefix + i + "=" + options.GetRecentProfile(i));
+        }
+        writer.WriteLine("FFmpegPath=" + options.FFmpegPath);
+        writer.WriteLine("FFmpegArguments=" + options.FFmpegArguments);
+        writer.WriteLine("TmpMainWindowLeft=" + options.TmpMainWindowLeft);
+        writer.WriteLine("TmpMainWindowTop=" + options.TmpMainWindowTop);
+        writer.WriteLine("TmpMainWindowWidth=" + options.TmpMainWindowWidth);
+        writer.WriteLine("TmpMainWindowHeight=" + options.TmpMainWindowHeight);
+        writer.WriteLine("TmpMainWindowState=" + options.TmpMainWindowState);
+        writer.WriteLine("TmpAreaIsExpanded=" + options.TmpAreaIsExpanded);
+        writer.WriteLine("TmpOptionsIsExpanded=" + options.TmpOptionsIsExpanded);
+        writer.WriteLine("TmpResizeMethodIsExpanded=" + options.TmpResizeMethodIsExpanded);
+        writer.WriteLine("TmpLayoutIsExpanded=" + options.TmpLayoutIsExpanded);
+        writer.WriteLine("AutoApply=" + options.AutoApply);
+        writer.WriteLine("LayoutPreview=" + options.LayoutPreview);
+        writer.WriteLine("LayoutBorder=" + options.LayoutBorder);
+        writer.WriteLine("LayoutSnap=" + options.LayoutSnap);
+        writer.WriteLine("TmpCompactView=" + options.TmpCompactView);
+        writer.WriteLine("ForceAeroOn=" + options.ForceAeroOn);
+        writer.WriteLine("TmpRestoreLastProfile=" + options.TmpRestoreLastProfile);
         return true;
       } catch (Exception ex) {
         // 特に何も警告はしない
@@ -171,167 +105,129 @@ public static class OptionsINIFile {
         // 空文字列なので読み飛ばす
         continue;
       }
-      var label = line.Substring(0, splitIndex);
+      var label = line.Substring(0, splitIndex).Trim();
       var rawData = line.Substring(splitIndex+1);
       labelToRawData.Add(label, rawData);
     }
 
-    // keyToLabelから値を設定していく
-    foreach (var keyAndLabel in keyToLabel) {
-      if (labelToRawData.ContainsKey(keyAndLabel.Value)) {
-        // すべてO(1)
-        var key = keyAndLabel.Key;
-        var rawData = labelToRawData[keyAndLabel.Value];
-        ParseRawData(options, key, rawData);
-      }
-    }
+    // ディクショナリを参考にしながらデータを設定
+    LoadFromDictionary(labelToRawData, options);
 
     return true;
   }
 
-  /// ファイル入力内容からメンバ変数に値を設定する
-  private static void ParseRawData(Options options, Key key, string rawData) {
-    // keyからメンバ変数に値を代入していく
-    switch (key) {
-      case Key.RecentProfile1: {
-        options.SetRecentProfile(0, rawData);
-        break;
+  private static void LoadFromDictionary(Dictionary<string, string> labelToRawData,
+                                         Options options) {
+    // 使いまわすので注意
+    string rawData;
+
+    // Dictionaryを調べながら値を設定する
+    string prefix = "RecentProfile";
+    for (int i = 0; i < 5; ++i) {
+      if (labelToRawData.TryGetValue(prefix + i, out rawData)) {
+        options.SetRecentProfile(i, rawData);
       }
-      case Key.RecentProfile2: {
-        options.SetRecentProfile(1, rawData);
-        break;
+    }
+    if (labelToRawData.TryGetValue("FFmpegPath", out rawData)) {
+      options.FFmpegPath = rawData;
+    }
+    if (labelToRawData.TryGetValue("FFmpegArguments", out rawData)) {
+      options.FFmpegArguments = rawData;
+    }
+    if (labelToRawData.TryGetValue("TmpMainWindowLeft", out rawData)) {
+      double parsedData;
+      if (double.TryParse(rawData, out parsedData)) {
+        options.TmpMainWindowLeft = parsedData;
       }
-      case Key.RecentProfile3: {
-        options.SetRecentProfile(2, rawData);
-        break;
+    }
+    if (labelToRawData.TryGetValue("TmpMainWindowTop", out rawData)) {
+      double parsedData;
+      if (double.TryParse(rawData, out parsedData)) {
+        options.TmpMainWindowTop = parsedData;
       }
-      case Key.RecentProfile4: {
-        options.SetRecentProfile(3, rawData);
-        break;
+    }
+    if (labelToRawData.TryGetValue("TmpMainWindowWidth", out rawData)) {
+      double parsedData;
+      if (double.TryParse(rawData, out parsedData)) {
+        options.TmpMainWindowWidth = parsedData;
       }
-      case Key.RecentProfile5: {
-        options.SetRecentProfile(4, rawData);
-        break;
+    }
+    if (labelToRawData.TryGetValue("TmpMainWindowHeight", out rawData)) {
+      double parsedData;
+      if (double.TryParse(rawData, out parsedData)) {
+        options.TmpMainWindowHeight = parsedData;
       }
-      case Key.FFmpegPath: {
-        options.FFmpegPath = rawData;
-        break;
+    }
+    if (labelToRawData.TryGetValue("TmpMainWindowState", out rawData)) {
+      Options.WindowState parsedData;
+      if (Enum.TryParse<Options.WindowState>(rawData, out parsedData)) {
+        options.TmpMainWindowState = parsedData;
       }
-      case Key.FFmpegArguments: {
-        options.FFmpegArguments = rawData;
-        break;
+    }
+    if (labelToRawData.TryGetValue("TmpAreaIsExpanded", out rawData)) {
+      bool parsedData;
+      if (bool.TryParse(rawData, out parsedData)) {
+        options.TmpAreaIsExpanded = parsedData;
       }
-      case Key.TmpMainWindowLeft: {
-        double parsedData;
-        if (double.TryParse(rawData, out parsedData)) {
-          options.TmpMainWindowLeft = parsedData; 
-        }
-        break;
+    }
+    if (labelToRawData.TryGetValue("TmpOptionsIsExpanded", out rawData)) {
+      bool parsedData;
+      if (bool.TryParse(rawData, out parsedData)) {
+        options.TmpOptionsIsExpanded = parsedData;
       }
-      case Key.TmpMainWindowTop: {
-        double parsedData;
-        if (double.TryParse(rawData, out parsedData)) {
-          options.TmpMainWindowTop = parsedData;
-        }
-        break;
+    }
+    if (labelToRawData.TryGetValue("TmpResizeMethodIsExpanded", out rawData)) {
+      bool parsedData;
+      if (bool.TryParse(rawData, out parsedData)) {
+        options.TmpResizeMethodIsExpanded = parsedData;
       }
-      case Key.TmpMainWindowWidth: {
-        double parsedData;
-        if (double.TryParse(rawData, out parsedData)) {
-          options.TmpMainWindowWidth = parsedData;
-        }
-        break;
+    }
+    if (labelToRawData.TryGetValue("TmpLayoutIsExpanded", out rawData)) {
+      bool parsedData;
+      if (bool.TryParse(rawData, out parsedData)) {
+        options.TmpLayoutIsExpanded = parsedData;
       }
-      case Key.TmpMainWindowHeight: {
-        double parsedData;
-        if (double.TryParse(rawData, out parsedData)) {
-          options.TmpMainWindowHeight = parsedData;
-        }
-        break;
+    }
+    if (labelToRawData.TryGetValue("AutoApply", out rawData)) {
+      bool parsedData;
+      if (bool.TryParse(rawData, out parsedData)) {
+        options.AutoApply = parsedData;
       }
-      case Key.TmpMainWindowState: {
-        Options.WindowState parsedData;
-        if (Enum.TryParse<Options.WindowState>(rawData, out parsedData)) {
-          options.TmpMainWindowState = parsedData;
-        }
-        break;
+    }
+    if (labelToRawData.TryGetValue("LayoutPreview", out rawData)) {
+      bool parsedData;
+      if (bool.TryParse(rawData, out parsedData)) {
+        options.LayoutPreview = parsedData;
       }
-      case Key.TmpAreaIsExpanded: {
-        bool parsedData;
-        if (bool.TryParse(rawData, out parsedData)) {
-          options.TmpAreaIsExpanded = parsedData;
-        }
-        break;
+    }
+    if (labelToRawData.TryGetValue("LayoutBorder", out rawData)) {
+      bool parsedData;
+      if (bool.TryParse(rawData, out parsedData)) {
+        options.LayoutBorder = parsedData;
       }
-      case Key.TmpOptionsIsExpanded: {
-        bool parsedData;
-        if (bool.TryParse(rawData, out parsedData)) {
-          options.TmpOptionsIsExpanded = parsedData;
-        }
-        break;
+    }
+    if (labelToRawData.TryGetValue("LayoutSnap", out rawData)) {
+      bool parsedData;
+      if (bool.TryParse(rawData, out parsedData)) {
+        options.LayoutSnap = parsedData;
       }
-      case Key.TmpResizeMethodIsExpanded: {
-        bool parsedData;
-        if (bool.TryParse(rawData, out parsedData)) {
-          options.TmpResizeMethodIsExpanded = parsedData;
-        }
-        break;
+    }
+    if (labelToRawData.TryGetValue("TmpCompactView", out rawData)) {
+      bool parsedData;
+      if (bool.TryParse(rawData, out parsedData)) {
+        options.TmpCompactView = parsedData;
       }
-      case Key.TmpLayoutIsExpanded: {
-        bool parsedData;
-        if (bool.TryParse(rawData, out parsedData)) {
-          options.TmpLayoutIsExpanded = parsedData; 
-        }
-        break;
+    }
+    if (labelToRawData.TryGetValue("ForceAeroOn", out rawData)) {
+      bool parsedData;
+      if (bool.TryParse(rawData, out parsedData)) {
+        options.ForceAeroOn = parsedData;
       }
-      case Key.AutoApply: {
-        bool parsedData;
-        if (bool.TryParse(rawData, out parsedData)) {
-          options.AutoApply = parsedData;
-        }
-        break;
-      }
-      case Key.LayoutPreview: {
-        bool parsedData;
-        if (bool.TryParse(rawData, out parsedData)) {
-          options.LayoutPreview = parsedData;
-        }
-        break;
-      }
-      case Key.LayoutBorder: {
-        bool parsedData;
-        if (bool.TryParse(rawData, out parsedData)) {
-          options.LayoutBorder = parsedData;
-        }
-        break;
-      }
-      case Key.LayoutSnap: {
-        bool parsedData;
-        if (bool.TryParse(rawData, out parsedData)) {
-          options.LayoutSnap = parsedData;
-        }
-        break;
-      }
-      case Key.TmpCompactView: {
-        bool parsedData;
-        if (bool.TryParse(rawData, out parsedData)) {
-          options.TmpCompactView = parsedData;
-        }
-        break;
-      }
-      case Key.ForceAeroOn: {
-        bool parsedData;
-        if (bool.TryParse(rawData, out parsedData)) {
-          options.ForceAeroOn = parsedData;
-        }
-        break;
-      }
-      case Key.TmpRestoreLastProfile: {
-        bool parsedData;
-        if (bool.TryParse(rawData, out parsedData)) {
-          options.TmpRestoreLastProfile = parsedData;
-        }
-        break;
+    }
+    if (labelToRawData.TryGetValue("TmpRestoreLastProfile", out rawData)) {
+      bool parsedData;
+      if (bool.TryParse(rawData, out parsedData)) {
+        options.TmpRestoreLastProfile = parsedData;
       }
     }
   }

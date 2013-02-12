efc9ee48d32ecf8ab85da0ad56002afe0a07fc62
Alalf <alalf.iQLc@gmail.com>
2013-02-12 18:08:59 +0900

    - 更に微調整。real-time buffer XXX% full! frame dropped!が発生したときに音ずれすることを確認。

--- tools/test_ffmpeg_webm.bat
+++ tools/test_ffmpeg_webm.bat
@@ -13,9 +13,9 @@ MKDIR "%OUTPUT_DIR%"
 DEL "%OUTPUT%"
 
 REM [WEBM]
-"%FFMPEG_EXE%" -rtbufsize 10MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -threads 4 -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -vcodec libvpx -preset medium -async 100 -acodec libvorbis -ar 48000 -ab 96k -ac 2 -vol 256 -f webm "%OUTPUT%"
+"%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -vcodec libvpx -preset medium -async 100 -acodec libvorbis -ar 48000 -ab 96k -ac 2 -vol 256 -threads 4 -metadata maxBitrate="700k" -f webm "%OUTPUT%"
 
 REM [MATROSKA]
-REM "%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -threads 4 -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -vcodec libvpx -preset medium -async 100 -acodec libvorbis -ar 48000 -ab 96k -ac 2 -vol 256 -f matroska "%OUTPUT%"
+REM "%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -vcodec libvpx -preset medium -async 100 -acodec libvorbis -ar 48000 -ab 96k -ac 2 -vol 256 -threads 4 -metadata maxBitrate="700k" -f matroska "%OUTPUT%"
 
 POPD

45bd6aef7fd0bcbb9aa74db0498b3ebd6c56bab1
Alalf <alalf.iQLc@gmail.com>
2013-02-16 19:39:25 +0900

    - VP80エンコーダが重すぎたので使うのを断念。H.264のMKVをいろいろテスト中。

--- tools/test_ffmpeg_webm.bat
+++ tools/test_ffmpeg_webm.bat
@@ -4,8 +4,8 @@ PUSHD "%ROOT_DIR%"
 
 SET OUTPUT_DIR=tools\tmp
 SET OUTPUT=http://localhost:8080/publish/first?password=secret
-SET OUTPUT_FILE=%OUTPUT_DIR%\test_ffmpeg_webm.webm
-REM SET OUTPUT_FILE=%OUTPUT_DIR%\test_ffmpeg_webm.mkv
+REM SET OUTPUT_FILE=%OUTPUT_DIR%\test_ffmpeg_webm.webm
+SET OUTPUT_FILE=%OUTPUT_DIR%\test_ffmpeg_webm.mkv
 SET VIDEO=SCFF DirectShow Filter
 SET AUDIO=Mixer (Creative SB X-Fi)
 SET FFMPEG_EXE=ext\ffmpeg\x64\bin\ffmpeg.exe
@@ -14,9 +14,9 @@ MKDIR "%OUTPUT_DIR%"
 DEL "%OUTPUT%"
 
 REM [WEBM]
-"%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -vcodec libvpx -preset medium -async 100 -acodec libvorbis -ar 48000 -ab 96k -ac 2 -vol 256 -threads 4 -metadata maxBitrate="700k" -f webm "%OUTPUT%" "%OUTPUT_FILE%"
+REM "%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -g 69 -vcodec libvpx -preset medium -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -async 100 -acodec libvorbis -ar 48000 -ab 96k -ac 2 -vol 256 -threads 4 -metadata maxBitrate="700k" -f webm "%OUTPUT%" "%OUTPUT_FILE%"
 
 REM [MATROSKA]
-REM "%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -vcodec libvpx -preset medium -async 100 -acodec libvorbis -ar 48000 -ab 96k -ac 2 -vol 256 -threads 4 -metadata maxBitrate="700k" -f matroska "%OUTPUT%" "%OUTPUT_FILE%"
+"%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -g 69 -vcodec libx264 -preset medium -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -async 100 -acodec libvorbis -ar 48000 -ab 96k -ac 2 -vol 256 -threads 4 -metadata maxBitrate="700k" -f matroska "%OUTPUT%" "%OUTPUT_FILE%"
 
 POPD

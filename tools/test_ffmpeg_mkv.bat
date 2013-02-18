844a23305a3731e5ffc60fcd56e6d4618982e669
Alalf <alalf.iQLc@gmail.com>
2013-02-18 09:20:47 +0900

    - ffmpeg配信テスト用スクリプトの更新

---
+++ tools/test_ffmpeg_mkv.bat
@@ -0,0 +1,21 @@
+@echo off
+SET ROOT_DIR=%~dp0..\
+PUSHD "%ROOT_DIR%"
+
+SET OUTPUT_DIR=tools\tmp
+SET OUTPUT=http://localhost:8080/publish/first?password=secret
+SET OUTPUT_FILE=%OUTPUT_DIR%\test_ffmpeg_webm.mkv
+SET VIDEO=SCFF DirectShow Filter
+SET AUDIO=Mixer (Creative SB X-Fi)
+SET FFMPEG_EXE=ext\ffmpeg\x64\bin\ffmpeg.exe
+
+MKDIR "%OUTPUT_DIR%"
+DEL "%OUTPUT%"
+
+REM [MATROSKA-H.264]
+"%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -g 69 -vcodec libx264 -preset medium -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -async 100 -acodec libvorbis -ar 48000 -ab 96k -ac 2 -vol 256 -threads 4 -metadata maxBitrate="700k" -f matroska "%OUTPUT%" "%OUTPUT_FILE%"
+
+REM [MATROSKA-Theora]
+REM "%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -g 69 -vcodec libtheora -preset medium -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -async 100 -acodec libvorbis -ar 48000 -ab 96k -ac 2 -vol 256 -threads 4 -metadata maxBitrate="700k" -f matroska "%OUTPUT%"
+
+POPD
--- tools/test_ffmpeg_webm.bat
+++ tools/test_ffmpeg_webm.bat
@@ -4,8 +4,7 @@ PUSHD "%ROOT_DIR%"
 
 SET OUTPUT_DIR=tools\tmp
 SET OUTPUT=http://localhost:8080/publish/first?password=secret
-REM SET OUTPUT_FILE=%OUTPUT_DIR%\test_ffmpeg_webm.webm
-SET OUTPUT_FILE=%OUTPUT_DIR%\test_ffmpeg_webm.mkv
+SET OUTPUT_FILE=%OUTPUT_DIR%\test_ffmpeg_webm.webm
 SET VIDEO=SCFF DirectShow Filter
 SET AUDIO=Mixer (Creative SB X-Fi)
 SET FFMPEG_EXE=ext\ffmpeg\x64\bin\ffmpeg.exe
@@ -14,9 +13,6 @@ MKDIR "%OUTPUT_DIR%"
 DEL "%OUTPUT%"
 
 REM [WEBM]
-REM "%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -g 69 -vcodec libvpx -preset medium -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -async 100 -acodec libvorbis -ar 48000 -ab 96k -ac 2 -vol 256 -threads 4 -metadata maxBitrate="700k" -f webm "%OUTPUT%" "%OUTPUT_FILE%"
-
-REM [MATROSKA]
-"%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -g 69 -vcodec libx264 -preset medium -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -async 100 -acodec libvorbis -ar 48000 -ab 96k -ac 2 -vol 256 -threads 4 -metadata maxBitrate="700k" -f matroska "%OUTPUT%" "%OUTPUT_FILE%"
+"%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -g 69 -vcodec libvpx -preset medium -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -async 100 -acodec libvorbis -ar 48000 -ab 96k -ac 2 -vol 256 -threads 4 -metadata maxBitrate="700k" -f webm "%OUTPUT%" "%OUTPUT_FILE%"
 
 POPD

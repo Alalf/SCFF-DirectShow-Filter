71dac787483c2de73c2fc6a483d7e0a2f8dd9913
Alalf <alalf.iQLc@gmail.com>
2013-02-18 16:32:08 +0900

    - ファイル出力とストリーム出力の形式で差が出るのでオプションを考え直さないといけないかも

--- tools/test_ffmpeg_mkv.bat
+++ tools/test_ffmpeg_mkv.bat
@@ -13,7 +13,11 @@ MKDIR "%OUTPUT_DIR%"
 DEL "%OUTPUT%"
 
 REM [MATROSKA-H.264]
-"%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -g 69 -vcodec libx264 -preset medium -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -async 100 -acodec libvorbis -ar 48000 -ab 96k -ac 2 -vol 256 -threads 4 -metadata maxBitrate="700k" -f matroska "%OUTPUT%" "%OUTPUT_FILE%"
+REM "%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -g 69 -vcodec libx264 -preset medium -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -async 100 -acodec libvorbis -ar 48000 -ab 96k -ac 2 -vol 256 -threads 4 -metadata maxBitrate="700k" -f matroska "%OUTPUT%"
+
+
+REM [MATROSKA-H.264/AAC]
+"%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -g 69 -vcodec libx264 -preset medium -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -async 100 -acodec libvo_aacenc -ar 48000 -ab 96k -ac 2 -vol 256 -threads 4 -metadata maxBitrate="700k" -f matroska "%OUTPUT%"
 
 REM [MATROSKA-Theora]
 REM "%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -g 69 -vcodec libtheora -preset medium -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -async 100 -acodec libvorbis -ar 48000 -ab 96k -ac 2 -vol 256 -threads 4 -metadata maxBitrate="700k" -f matroska "%OUTPUT%"
--- tools/test_ffmpeg_webm.bat
+++ tools/test_ffmpeg_webm.bat
@@ -13,6 +13,6 @@ MKDIR "%OUTPUT_DIR%"
 DEL "%OUTPUT%"
 
 REM [WEBM]
-"%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -g 69 -vcodec libvpx -preset medium -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -async 100 -acodec libvorbis -ar 48000 -ab 96k -ac 2 -vol 256 -threads 4 -metadata maxBitrate="700k" -f webm "%OUTPUT%" "%OUTPUT_FILE%"
+"%FFMPEG_EXE%" -rtbufsize 100MB -r 30 -s 640x360 -f dshow -i video="%VIDEO%":audio="%AUDIO%" -g 69 -vcodec libvpx -preset medium -vf fps="fps=30" -vb 700k -r 30 -s 640x360 -pix_fmt yuv420p -maxrate 700k -bufsize 1400k -crf 23 -qmin 10 -qmax 51 -async 10 -acodec libvorbis -ar 48000 -ab 96k -ac 2 -vol 256 -threads 4 -metadata maxBitrate="700k" -f webm "%OUTPUT%"
 
 POPD

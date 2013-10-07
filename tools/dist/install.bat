92f73d4b2edf36cb620b1e390ac9b511c602fc72
Alalf <alalf.iQLc@gmail.com>
2013-10-07 14:59:06 +0900

    - 64bitのOSで32bit版がインストールされていなかったのを修正

--- tools/dist/install.bat
+++ tools/dist/install.bat
@@ -41,6 +41,7 @@ rem ===================================================================
 rem ===================================================================
 set ROOT_DIR=%~dp0
 pushd "%ROOT_DIR%"
+regsvrex32 "Win32\scff_dsf_Win32.ax"
 regsvrex64 "x64\scff_dsf_x64.ax"
 popd
 exit /b

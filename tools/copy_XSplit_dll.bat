50216f53a0ee437a2edfabfb754423b23d719f25
Alalf <alalf.iQLc@gmail.com>
2013-10-09 21:19:21 +0900

    - XSplit用DSF作成のためのBATファイルを準備

---
+++ tools/copy_XSplit_dll.bat
@@ -0,0 +1,35 @@
+@ECHO OFF
+
+rem ===================================================================
+
+SET ROOT_DIR=%~dp0\
+PUSHD "%ROOT_DIR%"
+
+MKDIR "tmp\XSplit"
+COPY /Y "..\ext\XSplit\avcodec*.dll" "tmp\XSplit\"
+COPY /Y "..\ext\XSplit\avutil*.dll" "tmp\XSplit\"
+COPY /Y "..\ext\XSplit\swscale*.dll" "tmp\XSplit\"
+
+PUSHD "tmp\XSplit"
+CALL "%VS110COMNTOOLS%vsvars32.bat" >NUL:
+FOR %%f IN ("*.dll") DO dumpbin /EXPORTS "%%f" > "%%~nf.txt"
+FOR %%f IN ("*.txt") DO python "..\..\scripts\dllexports2def.py" %%f > "%%~nf.def"
+FOR %%f IN ("*.def") DO lib /MACHINE:X86 /DEF:"%%f"
+POPD
+
+MKDIR "..\ext\XSplit\lib"
+COPY /Y "tmp\XSplit\*.lib" "..\ext\XSplit\lib"
+COPY /Y "tmp\XSplit\*.exp" "..\ext\XSplit\lib"
+
+MKDIR "..\bin\Debug_Win32_XSplit\"
+MKDIR "..\bin\Release_Win32_XSplit\"
+
+COPY /Y "tmp\XSplit\avcodec*.dll" "..\bin\Debug_Win32_XSplit\"
+COPY /Y "tmp\XSplit\avutil*.dll" "..\bin\Debug_Win32_XSplit\"
+COPY /Y "tmp\XSplit\swscale*.dll" "..\bin\Debug_Win32_XSplit\"
+
+COPY /Y "tmp\XSplit\avcodec*.dll" "..\bin\Release_Win32_XSplit\"
+COPY /Y "tmp\XSplit\avutil*.dll" "..\bin\Release_Win32_XSplit\"
+COPY /Y "tmp\XSplit\swscale*.dll" "..\bin\Release_Win32_XSplit\"
+
+POPD

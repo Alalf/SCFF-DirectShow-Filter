84cf052bff1e45f93a6ff485edbc34dff7adf412
Alalf <alalf.iQLc@gmail.com>
2013-10-09 22:46:11 +0900

    - xsplit用プロジェクトの追加（将来的には削除予定）

--- tools/make_libs_for_XSplit.bat
+++ tools/make_libs_for_XSplit.bat
@@ -3,10 +3,23 @@ IF NOT DEFINED DevEnvDir CALL "%VS110COMNTOOLS%vsvars32.bat" >NUL:
 
 SET ROOT_DIR=%~dp0\
 SET DLL2DEF_PY=%ROOT_DIR%scripts\dllexports2def.py
+PUSHD "%ROOT_DIR%"
 
-PUSHD "%ROOT_DIR%\..\ext\XSplit\"
+PUSHD "..\ext\XSplit\"
 FOR %%f IN ("*.dll") DO dumpbin /EXPORTS "%%f" > "%%~nf.tmp"
 FOR %%f IN ("*.tmp") DO python "%DLL2DEF_PY%" %%f > "%%~nf.def"
 FOR %%f IN ("*.def") DO lib /MACHINE:X86 /DEF:"%%f"
 DEL "*.tmp"
+POPD
+
+MKDIR "..\bin\Debug_Win32_xsplit\"
+MKDIR "..\bin\Release_Win32_xsplit\"
+
+COPY /Y "..\ext\XSplit\avcodec*.dll" "..\bin\Debug_Win32_xsplit\"
+COPY /Y "..\ext\XSplit\avutil*.dll" "..\bin\Debug_Win32_xsplit\"
+COPY /Y "..\ext\XSplit\swscale*.dll" "..\bin\Debug_Win32_xsplit\"
+COPY /Y "..\ext\XSplit\avcodec*.dll" "..\bin\Release_Win32_xsplit\"
+COPY /Y "..\ext\XSplit\avutil*.dll" "..\bin\Release_Win32_xsplit\"
+COPY /Y "..\ext\XSplit\swscale*.dll" "..\bin\Release_Win32_xsplit\"
+
 POPD
